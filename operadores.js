c1a5c7517a31831f61dd361d588a2c541bea4e65
jonathanaraujou <jjau8085@gmail.com>
2023-07-30 19:18:52 -0300

    mi primer commit

---
+++ operadores.js
@@ -0,0 +1,56 @@
+// suma
+console.log(5 + 2);
+
+// resta
+console.log(4 - 1);
+
+// multiplicacion
+console.log(5 * 5);
+
+// division
+console.log(6 / 3);
+
+// módulo
+console.log(6 % 3);
+
+// incremento
+let numero = 6
+console.log(++numero);
+
+// decremento
+let otroNumero = 10
+console.log(--otroNumero);
+
+// comparacion simple
+console.log(5 == '5');
+
+// comparacion estricta
+console.log(5 === '5');
+
+// desigualdad simple
+console.log(5 != '5');
+
+// desigualdad estricta
+console.log(5 !== '5');
+
+// mayor que
+console.log(10 > 10);
+
+// mayor o igual que
+console.log(10 >= 10);
+
+// lo msmo pasa con menor y menor o igual que
+
+// and && (dará true si los dos se cumplen pero si uno no se cumpe dará falso)
+console.log(true && true);
+console.log(false && true);
+
+// or || va a pedir que aunque sea una sea verdadera, si ambas son falsas dará falso
+console.log(true || true);
+console.log(false || true);
+console.log(false || false);
+
+// concatenacion
+let nombre = 'jonathan';
+let apellido = 'araujo';
+console.log(nombre + ' ' + apellido);
---
+++ variablesYdatos.js
@@ -0,0 +1,67 @@
+console.log("mi prueba");
+console.log(typeof('mi prueba'));
+
+// variables var y let
+var telefonoJonathan = 1123878546;
+console.log(telefonoJonathan);
+let numero_preferido = 5;
+console.log(numero_preferido);
+
+// variable let en bloque cuando se repite el nombre con otro resultado
+if (true) {
+    let numero_preferido = 6;
+    console.log(numero_preferido);
+} 
+
+// constante es un tip de variable que no se puede cambiar, es permanente
+const empresa = 'Redes de Protección Bienestar';
+console.log(empresa);
+
+// datos del tipo number
+let edad = 42;
+console.log(edad);
+
+// datos del tipo number con decimales (debe colocarse punto para los decimales)
+let precio = 150.45;
+console.log(precio);
+
+// datos del tipo string con comillas dobles
+let nombre = "jonathan";
+console.log(nombre);
+
+// datos del tipo string con comillas simples
+let apellido = 'araujo';
+console.log(apellido);
+
+// datos booleano con valor true
+let verdadero = true;
+console.log(verdadero);
+
+// datos booleano con valor false
+let falso = false;
+console.log(falso);
+
+// datos del tipo NaN
+let notANumber = NaN;
+console.log(notANumber);
+
+// datos del tipo null
+let nulo = null;
+console.log(nulo);
+
+// dato undefined
+let sinValor;
+console.log(sinValor);
+
+// dato del tipo objeto literal
+let persona = {
+    nombre: 'jonathan',
+    edad: 42,
+    sexo: 'masculino'
+}
+console.log(persona);
+
+// datos del tipo array
+let paises = ['Venezuela', 'Argentina', 'Chile'];
+console.log(paises);
+
